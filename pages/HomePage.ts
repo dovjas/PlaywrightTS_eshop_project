09f60a0bf93fb858e24e1eb4caeb57ff11b3c12e
dovjas <dovydas.jaseliunas@gmail.com>
2026-07-07 11:51:55 +0300

    Created dynamic email

--- pages/HomePage.ts
+++ pages/HomePage.ts
@@ -1,17 +1,26 @@
-import {Page,Locator} from '@playwright/test';
+import { Page, Locator } from '@playwright/test';
 
 export class HomePage {
   //locators
   readonly page: Page;
   readonly signupLoginBtn: Locator;
-
+  readonly loggedInAsTxt: Locator;
+  readonly deleteAccBtn:Locator;
+  readonly accDeletedTxt:Locator
   //constructor
   constructor(page: Page) {
     this.page = page;
     this.signupLoginBtn = page.getByRole('link', { name: /Signup \/ Login/i });
+    this.loggedInAsTxt = page.locator('li b').last();
+    this.deleteAccBtn = page.locator('li:has-text(" Delete Account")');
+    this.accDeletedTxt = page.locator('[data-qa="account-deleted"]');
   }
   //actions
   async goToSignupLogin() {
     await this.signupLoginBtn.click();
+  }
+
+  async deleteAccount(){
+    await this.deleteAccBtn.click();
   }
 }
--- tests/signup.spec.ts
+++ tests/signup.spec.ts
@@ -6,12 +6,13 @@ import { SignupFormPage } from '../pages/auth/SignupFormPage.ts';
 import { AccountCreatedPage } from '../pages/auth/AccountCreatedPage.ts';
 import { testUser } from '../testData/users.ts';
 
-test('Test Case 1: User signup @signup', async ({ page }) => {
+test('Test Case 1: Register User @signup', async ({ page }) => {
   const homePage = new HomePage(page);
   const basePage = new BasePage(page);
   const signupLoginPage = new SignupLoginPage(page);
   const signupFormPage = new SignupFormPage(page);
   const accountCreatedPage = new AccountCreatedPage(page);
+  const email = `email+${Date.now()}+@test.com`
 
   // Step 1: Go to Home Page
   await basePage.navigate();
@@ -19,19 +20,35 @@ test('Test Case 1: User signup @signup', async ({ page }) => {
 
   // Step 2: Go to Signup/Login page
   await homePage.goToSignupLogin();
+
+  // Step 3: Verify 'New User Signup!' is visible
   expect(await signupLoginPage.headerText.textContent()).toContain(
     'New User Signup!',
   );
-  // Step 3: Enter Name and Email
-  await signupLoginPage.newUserSignup('Jonas', 'a@aj.lt');
+  // Step 4: Enter Name and Email
+  await signupLoginPage.newUserSignup('Jonas', email);
 
-  // Step 4: Fill full registration form
+  // Step 5: Verify that 'ENTER ACCOUNT INFORMATION' is visible
   expect(await signupFormPage.headerTxt.textContent()).toContain(
     'Enter Account Information',
   );
+
+  // Step 6: Fill full registration form
   await signupFormPage.completeRegistration(testUser);
+
+  // Step 7: Verify that 'ACCOUNT CREATED!' is visible
   expect(await accountCreatedPage.accountCreatedTxt.innerText()).toContain(
     'ACCOUNT CREATED!',
   );
   await accountCreatedPage.clickContinue();
+
+  // Step 8: Verify that 'Logged in as username' is visible
+  await expect(homePage.loggedInAsTxt).toBeVisible();
+
+  
+  // Step 9: Verify that 'ACCOUNT DELETED!' is visible and click 'Continue' button
+  await homePage.deleteAccount();
+  expect(await homePage.accDeletedTxt.textContent()).toContain(
+    'Account Deleted!',
+  );
 });
