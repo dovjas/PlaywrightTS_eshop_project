305cfd50264bade8ff1f39d3ca778073a7b5d90a
dovjas <dovydas.jaseliunas@gmail.com>
2026-07-08 12:50:03 +0300

    Log for bug fixing part 3

--- pages/base/BasePage.ts
+++ pages/base/BasePage.ts
@@ -13,20 +13,9 @@ export abstract class BasePage {
 
   async navigate(path: string = '/'): Promise<void> {
     const url = path.startsWith('http') ? path : `${this.baseUrl}${path}`;
-  console.log('Before goto');
-  console.log('Closed?', this.page.isClosed());
-
-  const response = await this.page.goto(url);
-
-  console.log('After goto');
-  console.log('Closed?', this.page.isClosed());
-  console.log('URL:', this.page.url());
-  console.log('Status:', response?.status());
-
     await this.page.goto(url, { waitUntil: 'domcontentloaded' });
-    await this.acceptConsentIfPresent();
-     console.log('After consent');
-     console.log('Closed?', this.page.isClosed());
+    // await this.acceptConsentIfPresent();
+
   }
 
   async acceptConsentIfPresent() {
