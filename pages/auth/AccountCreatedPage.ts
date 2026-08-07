55b7d3b5c4aed7a96847d461b3329bc9576745aa
dovjas <dovydas.jaseliunas@gmail.com>
2026-08-07 18:04:45 +0300

    Code improvement

--- pages/auth/AccountCreatedPage.ts
+++ pages/auth/AccountCreatedPage.ts
@@ -1,17 +1,18 @@
 import { Locator, Page } from '@playwright/test';
+import {BasePage} from '../base/BasePage';
 
-export class AccountCreatedPage {
-  page: Page;
+export class AccountCreatedPage extends BasePage {
   continueBtn: Locator;
   accountCreatedTxt: Locator;
 
   constructor(page: Page) {
-    this.page = page;
+    super(page);
+    
     this.continueBtn = page.locator('[data-qa="continue-button"]');
     this.accountCreatedTxt = page.locator('[data-qa="account-created"]');
   }
 
-  async clickContinue() {
+  async clickContinue():Promise<void> {
     await this.continueBtn.click();
   }
 }
