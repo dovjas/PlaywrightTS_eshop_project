83194aa5b8d19a10bce8d4e7d6aae61622b05ea1
dovjas <dovydas.jaseliunas@gmail.com>
2026-08-21 15:01:43 +0300

    created add block hadler

--- pages/base/BasePage.ts
+++ pages/base/BasePage.ts
@@ -4,18 +4,24 @@ export abstract class BasePage {
   protected page: Page;
   protected readonly baseURL: string;
   protected readonly consentBtn: Locator;
+  protected readonly closeAdBtn: Locator;
 
   constructor(page: Page) {
     this.page = page;
     this.baseURL = 'https://www.automationexercise.com/';
     this.consentBtn = page.getByRole('button', { name: 'Consent' });
+    this.closeAdBtn = page
+      .locator('.continue-prompt-text')
+      .or(page.frameLocator('iframe[name^="aswift_"]').locator('#dismiss-button'));
 
-    this.consenstHandler();
+    this.consentHandler();
+    // this.addHandler();
+    this.blockAds();
   }
 
   async navigate(path: string = '/'): Promise<void> {
     const url = path.startsWith('http') ? path : `${this.baseURL}${path}`;
-    console.log(`[BasePage] Navigating to: ${this.baseURL}`);
+    console.log(`[BasePage] Navigating to: ${url}`);
 
     try {
       await this.page.goto(url, {
@@ -28,12 +34,25 @@ export abstract class BasePage {
       throw err;
     }
   }
-  async consenstHandler(){
-    await this.page.addLocatorHandler(this.consentBtn, async()=>{
+  private consentHandler(): void {
+    this.page.addLocatorHandler(this.consentBtn, async () => {
       console.log(
         '[BasePage] Consent banner detected. Dismissing automatically...',
       );
       await this.consentBtn.click();
-    })
+    });
+  }
+
+  private addHandler():void {
+    this.page.addLocatorHandler(this.closeAdBtn, async () => {
+      console.log(
+        '[BasePage] Add banner detected. Dismissing automatically...',
+      );
+      await this.closeAdBtn.click();
+    });
+  }
+
+  private blockAds():void{
+    this.page.route('**/*google*/**',(route)=> route.abort());
   }
 }
