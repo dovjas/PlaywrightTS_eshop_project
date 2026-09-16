854c0a2ef3cd9bac45373df6cdbea395667d7aab
dovjas <dovydas.jaseliunas@gmail.com>
2026-09-16 20:18:48 +0300

    created global setup for auth

---
+++ tests/global.stetup.ts
@@ -0,0 +1,45 @@
+import { test as setup, expect } from '../fixtures/pomFixtures';
+import { testUser } from '../testData/users.ts';
+
+setup('Global - Signup auth', async ({ page, homePage, signupLoginPage }) => {
+  await setup.step('Step 1: Navigate to Home Page', async () => {
+    await homePage.navigate('/');
+  });
+  await setup.step(
+    'Step 2: Verify that home page is visible successfully',
+    async () => {
+      await expect(page).toHaveURL(/automationexercise.com/);
+      await expect(homePage.productCards.first()).toBeVisible();
+    },
+  );
+  await setup.step('Step 3: Go to Signup/Login page', async () => {
+    await homePage.goToSignupLogin();
+  });
+  await setup.step(
+    'Step 4: Verify "Login to your account" is visible',
+    async () => {
+      await expect(signupLoginPage.loginHeaderTxt).toContainText(
+        'Login to your account',
+      );
+    },
+  );
+  await setup.step('Step 5. Login using Email and Password ', async () => {
+    await signupLoginPage.userLogin(
+      testUser.validLoginUser.email,
+      testUser.validLoginUser.password,
+    );
+  });
+  await setup.step(
+    'Step 6. Verify that "Logged in as username" is visible',
+    async () => {
+      await expect(homePage.loggedInAsTxt).toBeVisible();
+      await expect(homePage.loggedInAsTxt).toContainText(
+        testUser.validLoginUser.firstName,
+      );
+    },
+  );
+
+  await page.context().storageState({
+    path: './playwright/.auth/auth.json',
+  });
+});
