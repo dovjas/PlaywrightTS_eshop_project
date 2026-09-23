aaed554bb1b1ae954b451d4076d0ae1b67b28561
dovjas <dovydas.jaseliunas@gmail.com>
2026-09-23 16:43:47 +0300

    Created BaseApiClient

--- src/api/base/BaseApiClient.ts
+++ src/api/base/BaseApiClient.ts
@@ -1,18 +1,26 @@
 import { APIRequestContext, APIResponse } from '@playwright/test';
 
-class BaseApiClient {
-  private readonly request: APIRequestContext;
-
-  constructor(request: APIRequestContext) {
-    this.request = request;
+export class BaseApiClient {
+  constructor(private readonly request: APIRequestContext) {
   }
 
-  /**
-   * Helper wrapper for POST requests with integrated step logging.
-   */
-  async post(
-    endpoint:string,
-    
-  )
+  async get(endpoint: string): Promise<APIResponse> {
+    return this.request.get(endpoint);
+  }
 
+  async post(endpoint: string, data?: object): Promise<APIResponse> {
+    return this.request.post(endpoint, {
+      data,
+    });
+  }
+
+  async put(endpoint: string, data?: object): Promise<APIResponse> {
+    return this.request.put(endpoint, {
+      data,
+    });
+  }
+
+  async delete(endpoint: string): Promise<APIResponse> {
+    return this.request.delete(endpoint);
+  }
 }
